95d313dc5897d4500617c0cdfdbe59acdd5239bd
hyesu <phs9575@gmail.com>
2023-06-01 15:13:38 +0900

    [refactoring] 35p

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,7 +1,7 @@
 #include "pch.h"
 #include "../Project8/prime-factors.cpp"
 using namespace std;
-TEST(TestCaseName, TestName) {
+TEST(TestCaseName, Of1) {
 	PrimeFactor prime_factor;
 	vector<int> expected = {};
 	EXPECT_EQ(expected, prime_factor.of(1));
